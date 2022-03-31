2be17a628fe392993bc98627a8116a3466ce0dd3
Kaique Santos <kaique.santos20042018@gmail.com>
2022-03-31 14:32:19 -0300

    Solicita dados do usuario e exibe o imc

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace CalculoImc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Escreva o seu nome: ");
+            string? nome = Console.ReadLine();
+
+            Console.WriteLine("Escreva a sua massa em kilogramas: ");
+            float massa = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Escreva o sua altura em metros: ");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            double imc = massa/(altura*altura);
+
+            Console.WriteLine("Olá {0} seu IMC é: {1}", nome, imc);
+
         }
     }
 }
